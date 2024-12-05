d85e045f1f071e8713b17bb9ae183414178806ef
yoann <yoann.ochietti@gmail.com>
2024-12-05 15:42:10 -0500

    Convert to side effect

--- src/drop-down.js
+++ src/drop-down.js
@@ -4,32 +4,28 @@ import "./drop-down.css";
 // throws error if target id does not exist
 // adds "drop-down-content" class to targets
 // adds/removes "visible" class on targets
-const setup = function setupEventHandlers() {
-  const dropDownTriggers = document.querySelectorAll(".drop-down-trigger");
+const dropDownTriggers = document.querySelectorAll(".drop-down-trigger");
 
-  for (let trigger of dropDownTriggers) {
-    const contentID = trigger.getAttribute("data-triggers-id");
-    if (contentID === null) {
-      throw new Error(`${trigger} needs a data-triggers-id to be set.`);
-    }
-    const contentNode = document.getElementById(contentID);
-    if (contentNode === null) {
-      throw new Error(`${contentID} needs needs to exist.`);
-    }
-
-    //avoids forcing user to add class manually, id is enough
-    if (!contentNode.classList.contains(".drop-down-content")) {
-      contentNode.classList.add("drop-down-content");
-    }
-
-    trigger.addEventListener("mouseenter", () => {
-      contentNode.classList.add("visible");
-    });
-
-    trigger.addEventListener("mouseleave", () => {
-      contentNode.classList.remove("visible");
-    });
+for (let trigger of dropDownTriggers) {
+  const contentID = trigger.getAttribute("data-triggers-id");
+  if (contentID === null) {
+    throw new Error(`${trigger} needs a data-triggers-id to be set.`);
   }
-};
+  const contentNode = document.getElementById(contentID);
+  if (contentNode === null) {
+    throw new Error(`${contentID} needs needs to exist.`);
+  }
 
-export { setup };
+  //avoids forcing user to add class manually, id is enough
+  if (!contentNode.classList.contains(".drop-down-content")) {
+    contentNode.classList.add("drop-down-content");
+  }
+
+  trigger.addEventListener("mouseenter", () => {
+    contentNode.classList.add("visible");
+  });
+
+  trigger.addEventListener("mouseleave", () => {
+    contentNode.classList.remove("visible");
+  });
+}
