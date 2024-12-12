52a83d04daeefe59a4afca40f1ceac722a645544
yoann <yoann.ochietti@gmail.com>
2024-12-12 14:58:05 -0500

    Export init function
    Allows rerunning module to scan for new drop downs. Useful for
    dynamically generated drop downs. Should perhaps have this be the only
    mode but have to adress testing in that case.

--- src/drop-down.js
+++ src/drop-down.js
@@ -4,28 +4,36 @@ import "./drop-down.css";
 // throws error if target id does not exist
 // adds "drop-down-content" class to targets
 // adds/removes "visible" class on targets
-const dropDownTriggers = document.querySelectorAll(".drop-down-trigger");
+function init() {
+  const dropDownTriggers = document.querySelectorAll(".drop-down-trigger");
 
-for (let trigger of dropDownTriggers) {
-  const contentID = trigger.getAttribute("data-triggers-id");
-  if (contentID === null) {
-    throw new Error(`${trigger} needs a data-triggers-id to be set.`);
+  for (let trigger of dropDownTriggers) {
+    const contentID = trigger.getAttribute("data-triggers-id");
+    if (contentID === null) {
+      throw new Error(`${trigger} needs a data-triggers-id to be set.`);
+    }
+    const contentNode = document.getElementById(contentID);
+    if (contentNode === null) {
+      throw new Error(`${contentID} needs needs to exist.`);
+    }
+
+    //avoids forcing user to add class manually, id is enough
+    if (!contentNode.classList.contains(".drop-down-content")) {
+      contentNode.classList.add("drop-down-content");
+    }
+
+    trigger.addEventListener("mouseenter", () => {
+      contentNode.classList.add("visible");
+    });
+
+    trigger.addEventListener("mouseleave", () => {
+      contentNode.classList.remove("visible");
+    });
   }
-  const contentNode = document.getElementById(contentID);
-  if (contentNode === null) {
-    throw new Error(`${contentID} needs needs to exist.`);
-  }
-
-  //avoids forcing user to add class manually, id is enough
-  if (!contentNode.classList.contains(".drop-down-content")) {
-    contentNode.classList.add("drop-down-content");
-  }
-
-  trigger.addEventListener("mouseenter", () => {
-    contentNode.classList.add("visible");
-  });
-
-  trigger.addEventListener("mouseleave", () => {
-    contentNode.classList.remove("visible");
-  });
 }
+
+//run on import
+init();
+
+//allow rerunning init if needed
+export { init };
